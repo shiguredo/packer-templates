0843279b35509e8ac17397448be3555bf2cf8284
akira-kuriyama <marronshepherd2@gmail.com>
2014-02-20 01:31:26 +0900

    Fix kernel-devel package path

--- centos-6.4/scripts/base.sh
+++ centos-6.4/scripts/base.sh
@@ -1,3 +1,5 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
+yum -y install gcc make gcc-c++ \
+		http://vault.centos.org/6.4/os/x86_64/Packages/kernel-devel-`uname -r`.rpm \
+		perl nfs-utils
 
