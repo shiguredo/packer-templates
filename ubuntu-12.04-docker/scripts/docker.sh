1203ae7dc1b035c646c1014ad77e5b41c8eacbdd
yuitowest <yuito@shiguredo.jp>
2014-04-13 13:47:42 +0900

    Use ShellScript that docker.io provides

--- ubuntu-12.04-docker/scripts/docker.sh
+++ ubuntu-12.04-docker/scripts/docker.sh
@@ -1,4 +1,1 @@
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sh -c 'echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list'
-apt-get -y update
-apt-get -y install lxc-docker
+wget -q -O - https://get.docker.io/ubuntu/ | sh
