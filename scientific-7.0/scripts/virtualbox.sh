e4abd99a094ada8fbfa301c96b361ce41d8bec8b
Shinya Ohyanagi <sohyanagi@gmail.com>
2014-10-16 00:39:12 +0900

    Add Scientific Linux 7.0

---
+++ scientific-7.0/scripts/virtualbox.sh
@@ -0,0 +1,7 @@
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
+
